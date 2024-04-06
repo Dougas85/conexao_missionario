ee24ff5499df7fdbc9cc1468731098c671970275
Dougas85 <149700231+Dougas85@users.noreply.github.com>
2024-04-06 10:07:41 -0300

    Update quiz.js
    inserção das questões diretamente no aplicação

--- quiz.js
+++ quiz.js
@@ -11,8 +11,11 @@ function loadQuestions() {
     if(isMock) {
         // Carrega perguntas de um array mockado se estiver no GitHub Pages
         questions = [
-            { id: 1, question: "Qual é a capital da França?", options: ["Paris", "Londres", "Berlim"], correct: "Paris" },
-            { id: 2, question: "Qual é o maior planeta do sistema solar?", options: ["Marte", "Júpiter", "Saturno"], correct: "Júpiter" },
+    { id: 1, question: "Qual é a capital da França?", options: ["Paris", "Londres", "Berlim"], correct: "Paris" },
+    { id: 2, question: "Qual é o maior planeta do sistema solar?", options: ["Marte", "Júpiter", "Saturno"], correct: "Júpiter" },
+    { id: 3, question: "Qual discípulo andou sobre as águas?", options: ["João", "Tiago", "Pedro", "Judas"], correct: "Pedro" },
+    { id: 4, question: "Quem foi o rei  mais jovem a reinar em Jerusalém?", options: ["Salomão", "Joás", "Josias", "Acaz"], correct: "Joás" },
+    { id: 5, question" "Qual é a capital de Roraima?" ["Porto Velho", "Rio Branco", "Boa Vista", "São Luiz" ], correct: "Boa Vista" },
             // Adicione mais perguntas conforme necessário
         ];
         displayQuestion();
